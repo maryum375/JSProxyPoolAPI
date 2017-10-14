251410917490a4fad588704edfaba88b73cb91ef
Ran Mariuma <maryum375@gmail.com>
2017-10-14 22:48:02 +0300

    Fixed pool call bug

--- api/endpoints/getproxies.js
+++ api/endpoints/getproxies.js
@@ -2,6 +2,7 @@ proxyPool = require("../../modules/proxy-pool");
 
 /* GET proxy listing. */
 var getProxy = function(req, res, next) {
+    proxyPool.getProxies(10, function(err, proxies) {
         if (err) {
             console.log(err);
             return;
