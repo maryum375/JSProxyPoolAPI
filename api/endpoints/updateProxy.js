55c667dd8d53df5a00ce37d4fae975c57563e810
Ran Mariuma <maryum375@gmail.com>
2017-10-19 14:44:21 +0300

    Added basic update endpoint (no validation on input)

--- api/endpoints/updateProxy.js
+++ api/endpoints/updateProxy.js
@@ -4,46 +4,89 @@ var Proxy = require("../../modules/proxy");
 /* GET proxy listing. */
 var updateProxy = function(req, res, next) {
     var proxy = new Proxy(req.query.ip, req.query.port);
+    if (req.query._address && req.query._port) {
+        return next("Unable to update _address or _port number of a proxy")
+    }
 
-    updateQuery = buildUpdateQuery(req.query);
-
-    // TODO Error handling
-    proxyPool.updateProxy(proxy, updateQuery);
-    res.send("Done");
+    // updateQuery = buildUpdateQuery(req.query);
+    performProxyUpdating(res, req.query.next);
 };
 
 
-let buildUpdateQuery = function(reqQuery) {
-    updateQuery = {}
+// let buildUpdateQuery = function(reqQuery) {
+//     updateQuery = {}
 
-    if (reqQuery.active) {
-        updateQuery._active = (reqQuery.active == 'true');
-    }
+//     // Contains the parameters and the validators for them
+//     let validations = {
+//         active: { validator: validateActivityInput, queryParamName: "_active" },
+//         lastCheckedTime: { validator: validateTimeInputParameter, queryParamName: "_lastCheckedTime" },
+//         active: { validator: validateActivityInput, queryParamName: "_active" },
+//         active: { validator: validateActivityInput, queryParamName: "_active" },
+//     }
 
-    if (reqQuery.lastCheckedTime) {
-        var lastCheckedTime = validateLastCheckedTimeInput(reqQuery.lastCheckedTime);
-        updateQuery._lastCheckedTime = lastCheckedTime;
-    }
-}
+//     if (reqQuery.active) {
+//         validateActivityInput(reqQuery.active, function(active) {
+//             updateQuery._active = active
+//         })
+//     }
 
-let validateActivityInput = function(active) {
-    return (req.query.active == 'true');
-}
 
-let validateLastCheckedTimeInput = function() {
+//     timeParamsNames = ["lastCheckedTime", "lastUsedTime"]
+//     for (var paramNameIndex = 0; paramNameIndex < timeParamsNames.length; paramNameIndex++) {
+//         let curParamName = timeParamsNames[paramNameIndex];
+//         validateTimeInputParameter(reqQuery[curParamName], function(err, formattedParamValue) {
+//             if (err) {
+//                 return next(err)
+//             }
+//             updateQuery["_" + curParamName] = formattedParamValue;
+//         })
+//     }
 
-    var lastCheckTime = parseFloat(lastCheckedTime);
-    if (isNaN(lastCheckTime)) {
-        next("lastCheckedTime param is not a number")
-    } else {
-        // TODO Error handling
-        proxyPool.updateProxyLastCheckTime(proxy, lastCheckTime, function(err, proxyfromDB) {
-            if (err) {
-                return next(err);
-            }
-            res.send(proxyfromDB);
-        });
-    }
+
+//     if (reqQuery.speed) {
+//         validateSpeedInput(reqQuery.speed, function(active) {
+//             updateQuery._speed = active
+//         })
+//     }
+
+
+//     performProxyUpdating(res, next);
+// }
+
+// let validateActivityInput = function(active, callback) {
+//     callback(req.query.active == 'true')
+// }
+
+// let validateSpeedInput = function(speed, callback) {
+//     let isValid = !isNaN(speed)
+//     callback(isValid)
+// }
+
+// let validateTimeInputParameter = function(timeParam, callback) {
+//     if (timeParam) {
+//         validateTimeInput(timeParam, function(err, lastCheckedTime) {});
+//     }
+// }
+
+// let validateTimeInput = function(timeInput, callback) {
+
+//     var formattedTimeInput = parseFloat(timeInput);
+//     if (isNaN(formattedTimeInput)) {
+//         callback("parameter should be a number. '" + timeInput + "' given")
+//     } else if (formattedTimeInput > Date.now()) {
+//         callback("time parameter should be in the past. '" + timeInput + "' given")
+//     } else {
+//         callback(null, formattedTimeInput)
+//     }
+// }
+
+let performProxyUpdating = function(res, updateQuery, next) {
+    proxyPool.updateProxy(proxy, updateQuery, function(err, proxyfromDB) {
+        if (err) {
+            return next(err);
+        }
+        res.send(proxyfromDB);
+    });
 }
 
 module.exports = updateProxy;
